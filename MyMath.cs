d81c8a8ceefea34e7951bad8ea759c0864fe3418
lukas <lukaschen761211@gmail.com>
2017-05-10 21:39:58 +0800

    Generic Sample

---
+++ MyMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    public class MyMath<T> where T: IComparable
+    {
+        public T Max(T x, T y)
+        {
+            if (x.CompareTo(y) > 0)
+                return x;
+            else
+                return y;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public static T Max<T>(T x, T y) where T:IComparable
+        {
+            if (x.CompareTo(y) > 0)
+                return x;
+            else
+                return y;
+        }
+        static void Main(string[] args)
+        {
+            //MyMath<int> obj = new MyMath<int>();
+            //var res = obj.Max(10, 20);
+            var res = Max<int>(10, 20);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
